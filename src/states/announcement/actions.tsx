714bde74298d047675c0eac26c8f4cb47b3644cc
keitakn <keita.koga.work@gmail.com>
2018-09-08 22:41:55 +0900

    :fire: #7 不要になったファイルを削除

--- src/states/announcement/actions.tsx
+++
@@ -1,5 +0,0 @@
-export const UPDATE_ANNOUNCEMENT = "[Announcement] update";
-
-export const updateAnnouncement = (message: string) => dispatch => {
-  return dispatch({ type: UPDATE_ANNOUNCEMENT, message });
-};
