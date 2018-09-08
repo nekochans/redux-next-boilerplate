708327c9d34c57f7ac45279d8555f9dfc266a453
keitakn <keita.koga.work@gmail.com>
2018-09-08 23:00:07 +0900

    :recycle: #7 Propsのデータ型を複数のStateを持っても大丈夫な型に変更

--- src/pages/counter.tsx
+++ src/pages/counter.tsx
@@ -3,14 +3,14 @@ import { ICounterState } from "../modules/Counter";
 import CounterContainer , { ActionDispatcher } from "../containers/Counter";
 
 interface IProps {
-  value: ICounterState;
+  counterState: ICounterState;
   actions: ActionDispatcher;
 }
 
 export const Counter: React.SFC<IProps> = (props: IProps) => {
   return (
     <>
-      <CounterContainer actions={props.actions} value={props.value} />
+      <CounterContainer actions={props.actions} value={props.counterState} />
     </>
   );
 };
