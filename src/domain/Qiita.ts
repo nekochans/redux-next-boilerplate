a740c98e1eead773d67d95880ac39a33b2194794
keitakn <keita.koga.work@gmail.com>
2018-09-18 00:02:13 +0900

    :sparkles: #5 QiitaAPIからユーザーを取得する関数を実装

--- src/domain/Qiita.ts
+++ src/domain/Qiita.ts
@@ -1,3 +1,6 @@
+// TODO APIサーバへ通信する場所はここではないどこかへ移動させる
+import axios, { AxiosResponse, AxiosError } from "axios";
+
 export interface IFetchQiitaUserRequest {
   id: string;
 }
@@ -11,3 +14,18 @@ export interface IFetchQiitaUserFailureResponse {
   // TODO このErrorは独自定義のErrorにするべき
   error: Error;
 }
+
+export const fetchUser = async (
+  request: IFetchQiitaUserRequest
+): Promise<IFetchQiitaUserResponse> => {
+  return axios
+    .get<IFetchQiitaUserResponse>(
+      `https://qiita.com/api/v2/users/${request.id}`
+    )
+    .then((axiosResponse: AxiosResponse) => {
+      return Promise.resolve(axiosResponse.data);
+    })
+    .catch((axiosError: AxiosError) => {
+      return Promise.reject(axiosError);
+    });
+};
