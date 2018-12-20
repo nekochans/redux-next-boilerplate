d0510202e1a673da1e1da21993c7c0dd91f97c36
keitakn <keita.koga.work@gmail.com>
2018-12-21 00:14:28 +0900

    :heavy_minus_sign: #57 dotenvを利用しない形になったのでpackageから削除ついでに以下の変更も実施
    - packageを最新状態に更新
    - 一部コンパイルが通らない箇所があったので修正

--- next.config.js
+++ next.config.js
@@ -1,21 +1,4 @@
-require("dotenv").config();
-const Dotenv = require("dotenv-webpack");
-const path = require("path");
 const withTypescript = require("@zeit/next-typescript");
 const withSass = require("@zeit/next-sass");
 
-const webpackModule = {
-  webpack: (config) => {
-    config.plugins = [
-      ...config.plugins,
-      new Dotenv({
-        path: path.join(__dirname, ".env"),
-        systemvars: true,
-      }),
-    ];
-
-    return config;
-  },
-};
-
-module.exports = withTypescript(withSass(webpackModule));
+module.exports = withTypescript(withSass());
